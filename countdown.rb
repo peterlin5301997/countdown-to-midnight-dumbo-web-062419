cdeb6e8c01ded8b15cc140c9a57451adb5c7954a
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-17 18:49:36 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(number)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  sleep(5.seconds)
+  while number > 0 do
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
